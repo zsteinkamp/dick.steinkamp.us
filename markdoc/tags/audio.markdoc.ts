9730ea5135eb497f7228c0c97809f4f85eb5734c
Zack Steinkamp <zack@steinkamp.us>
2023-04-30 10:26:52 -0700

    initial commit

---
+++ markdoc/tags/audio.markdoc.ts
@@ -0,0 +1,10 @@
+import Audio from '@/components/Audio';
+
+export const audio = {
+  render: Audio,
+  attributes: {
+    src: { type: String },
+    type: { type: String, default: "audio/wav" },
+    className: { type: String },
+  },
+};
---
+++ pages/studeute/2008-10-06_hot-rod-deluxe.md
@@ -0,0 +1,9 @@
+---
+layout: post
+title: Hot Rod Deluxe
+date: "2008-10-06"
+---
+
+A nice mention about the Ute in the latest issue of Hot Rod Deluxe by the editor, Jerry Pitt…
+
+![](/images/pop/studeute/HRD.jpg "HRD")
