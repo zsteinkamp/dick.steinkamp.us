41e43d310c00428f2355c03e03c35998b4d9e3f3
Zack Steinkamp <zack@steinkamp.us>
2023-05-02 18:05:02 -0700

    show text in timeline

--- pages/timeline.tsx
+++ pages/timeline.tsx
@@ -2,6 +2,7 @@ import fsp from "fs/promises";
 import path from "path";
 import yaml from "js-yaml";
 import Image from "next/image";
+import Markdoc from '@markdoc/markdoc';
 
 export const getStaticProps = async () => {
   const data = yaml.load(await (fsp.readFile(path.join(process.cwd(), "pages", "timeline.yml"))));
@@ -15,12 +16,19 @@ export const getStaticProps = async () => {
 
 const Timeline = ({ data }) => {
   const timeline = data.map((project, i) => {
+    const ast = Markdoc.parse(project.desc || '');
+    const content = Markdoc.transform(ast);
+
     return (
       <div key={project.image} className={`timelineRow ${i % 2 ? "even" : "odd"}`}>
         <div className="projYear">{project.projYear}</div>
-        <div>
-          <h2 className="projTitle font-condensed text-2xl mb-2 mt-2">{project.year} {project.title}</h2>
-          <div className="projImage"><Image className="" src={`/images/timeline/${project.image}`} width="384" height="384" alt={project.title} /></div>
+        <div className="projContent flex flex-row grow">
+          <div className="basis-5/12 projText">
+            <h2 className="projTitle font-condensed text-2xl mb-2 mt-2">{project.year} {project.title}</h2>
+            <div className="projDesc" dangerouslySetInnerHTML={{__html: Markdoc.renderers.html(content)}}></div>
+          </div>
+          <div className="basis-2/12"></div>
+          <div className="basis-5/12 projImage"><Image className="" src={`/images/timeline/${project.image}`} width="384" height="384" alt={project.title} /></div>
         </div>
       </div>
     )});
