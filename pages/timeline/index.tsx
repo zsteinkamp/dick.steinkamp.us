a6626fd14c2caaba7254850709f471c94d35c80d
Zack Steinkamp <zack@steinkamp.us>
2023-06-10 10:24:20 -0700

    improvements

--- pages/timeline/index.tsx
+++ pages/timeline/index.tsx
@@ -2,6 +2,7 @@ import fsp from "fs/promises";
 import path from "path";
 import yaml from "js-yaml";
 import Image from "next/image";
+import Link from "next/link";
 import Markdoc from '@markdoc/markdoc';
 
 export const getStaticProps = async () => {
@@ -26,6 +27,7 @@ const Timeline = ({ data }) => {
           <div className="basis-5/12 projText">
             <h2 className="projTitle font-condensed text-2xl mb-2 mt-2">{project.year} {project.title}</h2>
             <div className="projDesc" dangerouslySetInnerHTML={{__html: Markdoc.renderers.html(content)}}></div>
+            { project.moreLink && <Link href={project.moreLink}>More info...</Link>}
           </div>
           <div className="basis-2/12"></div>
           <div className="basis-5/12 projImage"><Image className="" src={`/images/timeline/${project.image}`} width="384" height="384" alt={project.title} /></div>
